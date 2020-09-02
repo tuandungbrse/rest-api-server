f077391f43ccaef796a50f5ba46ed540aa5c52a9
tuandungbrse <tuandungbrse@gmail.com>
2020-09-03 02:34:14 +0700

    setting up socket.io for real time

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
-
 var app = require('./app');
+var http = require('http');
 
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
-  .then(() => {
-    app.listen(8080);
-  })
-  .catch((error) => console.log(error));
+const init = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+    var server = http.createServer(app);
+    var io = require('socket.io')(server);
+    io.on('connection', (socket) => {
+      console.log('A client just connected', socket);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
