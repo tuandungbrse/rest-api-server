880384b24a2b5a797b6584d2aa26bd598a96c10d
tuandungbrse <tuandungbrse@gmail.com>
2020-09-03 23:02:31 +0700

    removing socket io

--- src/index.js
+++ src/index.js
@@ -11,10 +11,6 @@ const init = async () => {
     });
     var server = http.createServer(app);
     server.listen(8080);
-    var io = require('./socket').init(server);
-    io.on('connection', (socket) => {
-      console.log('A client just connected', socket);
-    });
   } catch (error) {
     console.log(error);
   }
